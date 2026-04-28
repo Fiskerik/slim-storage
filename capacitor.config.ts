ed46a1e856408853e2e2937fe626515189345362
Fiskerik <63838726+Fiskerik@users.noreply.github.com>
2026-04-28 09:09:47 +0000

    add

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.fiskerik.trimswipe',
+  appName: 'TrimSwipe',
+  webDir: 'dist'
+};
+
+export default config;
