6ee31a9c8df09c0a488dcbbdb5711a7f80252751
Fiskerik <63838726+Fiskerik@users.noreply.github.com>
2026-05-04 11:42:05 +0000

    rensat

--- index.js
+++ index.js
@@ -1,32 +1,17 @@
 import registerRootComponent from 'expo/src/launch/registerRootComponent';
-import { WebView } from 'react-native-webview';
 import React from 'react';
-import { SafeAreaView, StyleSheet, StatusBar, Platform } from 'react-native';
+import { WebView } from 'react-native-webview';
+import { SafeAreaView, StyleSheet } from 'react-native';
 
 const App = () => {
   return (
-    <SafeAreaView style={styles.container}>
-      <StatusBar barStyle="dark-content" />
-      <WebView
+    <SafeAreaView style={{ flex: 1, backgroundColor: '#fff' }}>
+      <WebView 
         source={{ uri: 'https://trimswipe.lovable.app' }} 
         style={{ flex: 1 }}
-        startInLoadingState={true}
-        // Tillåt inline-video och andra mobilfunktioner
-        allowsInlineMediaPlayback={true}
-        domStorageEnabled={true}
-        javaScriptEnabled={true}
       />
     </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  container: { 
-    flex: 1, 
-    backgroundColor: '#fff',
-    // Hanterar padding för notch/statusfält på iOS
-    paddingTop: Platform.OS === 'ios' ? 0 : StatusBar.currentHeight 
-  }
-});
 
 registerRootComponent(App);
