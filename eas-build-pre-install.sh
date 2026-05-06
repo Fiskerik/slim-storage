df34c8e539000d16921d08466429a6c91be7b5c9
Fiskerik <63838726+Fiskerik@users.noreply.github.com>
2026-05-06 13:11:48 +0000

    Fix

--- eas-build-pre-install.sh
+++ eas-build-pre-install.sh
@@ -1,2 +1,9 @@
-#!/bin/bash
-npm install
+#!/usr/bin/env bash
+
+echo "=== EAS Pre-Install Hook ==="
+
+if [[ "$EAS_BUILD_PLATFORM" == "ios" ]]; then
+  echo "Installing cmake and pkg-config for ReactNativeStaticServer..."
+  brew install cmake pkg-config
+  echo "CMake version: $(cmake --version)"
+fi
